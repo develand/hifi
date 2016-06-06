28adaca666dc4d0e12a5d120b78f3c1091482507
Doug Eveland <doug@curemetrix.com>
2016-06-06 07:09:35 -0700

    Added support for accounts collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
- 
+import React, {Component, PropTypes} from 'react';
+import {createContainer} from 'meteor/react-meteor-data';
 import Account from './Account.jsx';
+import {Accounts} from '../api/Accounts.js'
  
 // App component - represents the whole app
 export default class App extends Component {
-  getAccounts() {
-    return [
-      { _id: 1, text: 'C234223423' },
-      { _id: 2, text: 'C123123131' },
-      { _id: 3, text: 'C888234232' },
-    ];
-  }
  
   renderAccounts() {
-    return this.getAccounts().map((account) => (
+    return this.props.accounts.map((account) => (
       <Account key={account._id} account={account} />
     ));
   }
@@ -32,3 +26,13 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  accounts: PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+  return {
+    accounts: Accounts.find({}).fetch()
+  }
+}, App);
