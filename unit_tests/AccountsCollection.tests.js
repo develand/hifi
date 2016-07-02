89926c14a9153b6a6fea3e555eeebaf58d2285a2
Doug Eveland <doug@curemetrix.com>
2016-07-02 09:33:31 -0700

    Small changes to Accounts Collection tests

--- unit_tests/AccountsCollection.tests.js
+++ unit_tests/AccountsCollection.tests.js
@@ -1,6 +1,7 @@
 import {Meteor} from 'meteor/meteor';
 import {assert, expect} from 'meteor/practicalmeteor:chai';
 import {AccountsCollection} from '../imports/api/AccountsCollection.js';
+import {Promise} from 'meteor/promise';
 
 if (Meteor.isServer) {
   describe('Accounts', function() {
@@ -34,5 +35,49 @@ if (Meteor.isServer) {
           'meteor.accounts.$accountNumber_1  dup key: { : "XX1234" }');
       });
     });
+  });
+
+  describe('Account Assets', function() {
+      let account = null;
+
+      beforeEach(function() {
+        console.log('before each');
+        AccountsCollection.remove({});
+        AccountsCollection.insert({
+          accountNumber: 'XX1234',
+          createdAt: new Date(),
+          balance: 100,
+          assets: [
+            {
+              symbol : "IBM",
+              numUnits: 1000,
+              purchaseDate: "2015-01-01",
+              unitPrice: 150.00,
+            },
+            {
+              symbol : "IBM",
+              numUnits: 1000,
+              purchaseDate: "2015-01-01",
+              unitPrice: 150.00,
+            },
+            {
+              symbol : "APL",
+              numUnits: 200,
+              purchaseDate: "2015-06-01",
+              unitPrice: 65.00,
+            },
+          ],
+
+        });
+
+        let account = AccountsCollection.findOne({acccountNumber: 'XX1234'});
+        console.log('Account: ' + account);
+      });
+
+      it('are saved and retrieved', () => {
+        expect(account).to.not.be.null;
+        assert.equal(account.accountNumber, 'XX1234');
+        assert.equal(account.assets.length, 3);
+      });
   });
 }
