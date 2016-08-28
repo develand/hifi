3bc7c8587cef673ead36b0c7353c25f8e72d1096
Doug Eveland <doug@curemetrix.com>
2016-08-28 06:23:48 -0700

    refactored app tests:

--- imports/ui/containers/App.tests.jsx
+++ imports/ui/containers/App.tests.jsx
@@ -4,6 +4,7 @@ import {expect} from 'meteor/practicalmeteor:chai';
 import {mount} from 'enzyme';
 import {IntlProvider} from 'react-intl';
 import App from './App.jsx'
+import AccountOverview from '../components/AccountOverview.jsx'
 import {Grid, Row, Col, Tabs, Tab} from 'react-bootstrap';
 
 if (Meteor.isClient) {
@@ -19,19 +20,18 @@ if (Meteor.isClient) {
  
                     appContainerElem =
                         mount(<IntlProvider locale="en"><App /></IntlProvider>);
-                    tabs = appContainerElem.find(Tabs);
                 });
 
                 it('the overview tab lists all selected accounts in order by account number', () => {
-                    const overviewTabElem = tabs.find(Tab).at(0);
-                    const gridElem = overviewTabElem.find(Grid);
-                    const gridRows = gridElem.find(Row)
-                    const firstDataRow = gridRows.at(1);
-                    let firstCol = firstDataRow.find(Col);
-                    expect(firstCol.find('div').at(0).props.children).to.contain(FIRST_ACCOUNT_NUMBER);
-                    const secondDataRow = gridRows.at(2);
-                    firstCol = secondDataRow.find(Col);
-                    expect(firstCol.find('div').at(0).props.children).to.contain(SECOND_ACCOUNT_NUMBER);
+                    const accountOverviewTab = appContainerElem.find(AccountOverview);
+
+                    const firstDataRow = accountOverviewTab.find(Row).at(1)
+                    const firstRowCols = firstDataRow.find(Col);
+                    expect(firstRowCols.at(0).find('div').at(0).props.children).to.contain(FIRST_ACCOUNT_NUMBER);
+
+                    const secondDataRow = accountOverviewTab.find(Row).at(2)
+                    const secondRowCols = secondDataRow.find(Col);
+                    expect(secondRowCols.at(0).find('div').at(0).props.children).to.contain(SECOND_ACCOUNT_NUMBER);
                 });
             });
         });
